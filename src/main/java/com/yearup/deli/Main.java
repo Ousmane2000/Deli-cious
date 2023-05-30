0dca995c8af46bd18ac7c63b6e14b26ed89b277d
DaSaltySquid <104535317+DaSaltySquid@users.noreply.github.com>
2023-05-30 15:35:56 -0400

    Solved small bug, added addDrink menu(Partial)

--- src/main/java/com/yearup/deli/Main.java
+++ src/main/java/com/yearup/deli/Main.java
@@ -10,17 +10,17 @@ public class Main {
         while (running) {
             System.out.println("===== Welcome to the DELI-cious Home Screen =====");
             System.out.println("Choose an option: ");
-            System.out.println("A) New Order");
-            System.out.println("B) Exit");
+            System.out.println("1) New Order");
+            System.out.println("2) Exit");
 
-            String input = scanner.nextLine().trim();
+            int input = scanner.nextInt();
 
-            switch (input.toUpperCase()) {
-                case "A":
+            switch (input) {
+                case 1:
                     newOrder(scanner);
                     break;
 
-                case "B":
+                case 2:
                     running = false;
                     break;
 
@@ -38,32 +38,32 @@ public class Main {
         while (running) {
             System.out.println("===== Welcome to the Order Screen =====");
             System.out.println("Please choose option: ");
-            System.out.println("A) Add Sandwich");
-            System.out.println("B) Add Drink");
-            System.out.println("C) Add Chips");
-            System.out.println("D) Checkout");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
             System.out.println("0) Cancel Order");
 
-            String input = scanner.nextLine().trim();
+            int input = scanner.nextInt();
 
-            switch (input.toUpperCase()) {
-                case "A":
+            switch (input) {
+                case 1:
                     addSandwich(scanner);
                     break;
 
-                case "B":
+                case 2:
                     addDrink(scanner);
                     break;
 
-                case "C":
+                case 3:
                     addChips(scanner);
                     break;
 
-                case "D":
+                case 4:
                     checkout(scanner);
                     break;
 
-                case "0":
+                case 5:
                     running = false;
 
                 default:
@@ -80,30 +80,27 @@ public class Main {
         boolean running = true;
         while (running) {
             System.out.println("----- Choose an Option -----");
-            System.out.println("A) Select Sandwich Size: ");
-            System.out.println("B) Select Bread: ");
-            System.out.println("C) Select Toppings: ");
-            System.out.println("D) Toasted? y/n");
+            System.out.println("1) Select Sandwich Size: ");
+            System.out.println("2) Select Bread: ");
+            System.out.println("3) Select Toppings: ");
+            System.out.println("4) Toasted? y/n");
 
-            String input = scanner.nextLine().trim();
+            int input = scanner.nextInt();
 
-            switch (input.toUpperCase()) {
-                case "A":
+            switch (input) {
+                case 1:
                     sandwichSizeOption(scanner);
                     break;
 
-                case "B":
+                case 2:
                     breadOption(scanner);
                     break;
 
-                case "C":
+                case 3:
                     toppingOption(scanner);
                     break;
 
-                case "D":
-                    break;
-
-                case "0":
+                case 4:
                     running = false;
 
                 default:
@@ -116,57 +113,100 @@ public class Main {
 
     private static void sandwichSizeOption(Scanner scanner) {
         System.out.println("----- Enter Sandwich Size -----");
-        System.out.println("A) 4 inch Price is: $5.50");
-        System.out.println("B) 8 inch Price is: $7.00");
-        System.out.println("C) 12 inch Price is: $8.50");
+        System.out.println("1) 4 inch Price is: $5.50");
+        System.out.println("2) 8 inch Price is: $7.00");
+        System.out.println("3) 12 inch Price is: $8.50");
 
-        String input = scanner.nextLine().trim();
+        int input = scanner.nextInt();
     }
 
     private static void breadOption(Scanner scanner) {
         System.out.println("----- Enter Bread Type ----- ");
-        System.out.println("A) White");
-        System.out.println("B) Wheat");
-        System.out.println("C) Rye");
-        System.out.println("D) Wrap");
+        System.out.println("1) White");
+        System.out.println("2) Wheat");
+        System.out.println("3) Rye");
+        System.out.println("4) Wrap");
 
-        String input = scanner.nextLine().trim();
+        int input = scanner.nextInt();
     }
 
     private static void toppingOption(Scanner scanner) {
-        System.out.println("----- Enter Toppings of Choice -----");
-        System.out.println("Which Meat Would You Like?");
-        System.out.println("Steak, Ham, Salami, Roast Beef, Chicken, Bacon");
+        boolean running = true;
+        while (running) {
+            System.out.println("----- Enter Toppings of Choice -----");
+            System.out.println("Which Meat Would You Like?");
+            System.out.println("1) Steak");
+            System.out.println("2) Ham");
+            System.out.println("3) Salami");
+            System.out.println("4) Roast Beef");
+            System.out.println("5) Chicken");
+            System.out.println("6) Bacon");
 
-        String input = scanner.nextLine().trim();
-        
-        if (input.equals("4 inch")) {
-            System.out.println("Price: +$1.00");
-        } else if (input.equals("8inch")) {
-            System.out.println("Price: +$2.00");
-        } else if (input.equals("12inch")) {
-            System.out.println("Price: +$3.00");
+            int input = scanner.nextInt();
+
+            switch (input) {
+                case 1:
+                    sandwichSizeOption(scanner);
+                    break;
+
+                case 2:
+                    breadOption(scanner);
+                    break;
+
+                case 3:
+                    toppingOption(scanner);
+                    break;
+
+                case 4:
+                    running = false;
+
+                default:
+                    System.out.println("Invalid Option");
+                    break;
+            }
+
+            String input = scanner.nextLine().trim();
+
+            if (input.equals("4 inch")) {
+                System.out.println("Price: +$1.00");
+            } else if (input.equals("8inch")) {
+                System.out.println("Price: +$2.00");
+            } else if (input.equals("12inch")) {
+                System.out.println("Price: +$3.00");
+            }
+
         }
-
     }
 
 
-        private static void addDrink (Scanner scanner){
+        private static void addDrink (Scanner scanner) {
             boolean running = true;
             while (running) {
-                System.out.println("A) Select Drink Size: ");
-                System.out.println("B) Select Drink Flavor: ");
+                System.out.println("----- Select Drinks -----");
+                System.out.println("1) Coca Cola");
+                System.out.println("2) Pepsi");
+                System.out.println("3) Sprite");
+                System.out.println("4) Fanta");
+                System.out.println("5) Water");
 
-                String input = scanner.nextLine().trim();
+                int input = scanner.nextInt();
+                int selection = scanner.nextInt();
+
+                switch (selection) {
+                    case 1:
+
+                }
 
 
             }
         }
 
+
+
         private static void addChips (Scanner scanner){
             boolean running = true;
             while (running) {
-                System.out.println("A) Select Chips Type:");
+                System.out.println("1) Select Chips Type:");
 
                 String input = scanner.nextLine().trim();
 
@@ -177,9 +217,9 @@ public class Main {
         private static void checkout (Scanner scanner){
             boolean running = true;
             while (running) {
-                System.out.println("A) Select Chips Type:");
 
-                String input = scanner.nextLine().trim();
+
+
 
 
             }
